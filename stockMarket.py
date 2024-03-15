37bbf4070627e571468aa6dec09baac003d67a79
Veronique Fick <77314948+Veroniique@users.noreply.github.com>
2024-03-15 11:22:37 -0700

    Update stockMarket.py

--- stockMarket.py
+++ stockMarket.py
@@ -3,23 +3,15 @@
 Objective: Stock Market simulator. Tells user to buy, sell, stocks.
            Monitors their portfolio, traces historical prices of
            a set of stocks.
-TODO: - Show history of price(s)
-    - Create a quantity list to show shares owned by user and all previous purchases
-    - Create functions: - transactions
-                        - record transaction
-                        - random walk for adjusting stock prices after transaction
-    - Save user data
-    - Add error handling
+TODO: -Incorporate random walk function for random simulation of prices
     - Include strtlower on all inputs!!! (validation)
 '''
+
 #imported libraries
 import random
 
 #user money
 user_money = 15000
-
-#stock dictionary with name and price list
-stock_data = {}
 
 #Function that loads up the user portfolio
 def load_portfolio():
@@ -30,11 +22,13 @@ def load_portfolio():
                 stock_name, quantity, total_cost = line.strip().split(',')
                 portfolio[stock_name] = {'quantity': int(quantity), 'total_cost': float(total_cost)}
     except FileNotFoundError:
-        #If file is not found, it wont do anything
-        pass
+        print("User portfolio.txt not found")
+    except ValueError:
+        print("Incorrect Data")
+
     return portfolio
 
-#Saves users portfolio
+#Function that saves user stock info to external txt sheet
 def save_portfolio(portfolio):
     with open('user_portfolio.txt', 'w') as file:
         for stock, data in portfolio.items():
@@ -42,6 +36,99 @@ def save_portfolio(portfolio):
 
 #Load user portfolio from file
 user_portfolio = load_portfolio()
+
+#Loads up the stock file with current stock and prices
+def load_stockdata():
+    #Empty dict
+    stock_data = {}
+    # Task 2 - Open stock.txt file to read data
+    with open('stock.txt', 'r') as file:
+        # read each line
+        for line in file:
+            # split data in stock name and stock price
+            stock_name, initial_price = line.strip().split(',')
+
+            # store stock data in dictionary
+            stock_data[stock_name] = float(initial_price)
+
+    return stock_data
+
+#Function that allows user to buy stock
+def buy_stock(stock_data, stock_name, share_amount, user_money, user_portfolio):
+    #check if stock exists in stock data
+    if stock_name in stock_data:
+        #gets price of stock
+        stock_price = stock_data[stock_name]
+
+        # cost calculations for buying stocks
+        total_cost = stock_price * share_amount
+
+        # check if user has enough money
+        if total_cost <= user_money:
+            #deduct money from user funds
+            user_money -= total_cost
+            if stock_name in user_portfolio:
+                # update user portfolio
+                user_portfolio[stock_name]['quantity'] += share_amount
+                user_portfolio[stock_name]['total_cost'] += total_cost
+            else:
+                user_portfolio[stock_name] = {'quantity': share_amount, 'total_cost': total_cost}
+            # display how much bought of stock by price and total remaining
+            print(f"You have bought {share_amount} of {stock_name} for {stock_price} per share")
+            print(f"Total cost: ${total_cost}")
+            print(f"Your remaining total: ${user_money}")
+        else:
+            print("You don't have enough money to buy this")
+    else:
+        print("Stock not found in the stock data.")
+
+    return user_money, user_portfolio
+
+# Function that allows user to sell their stock(s)
+def sell_stock(stock_data, stock_name, share_amount, user_money, user_portfolio):
+    #check if stock exists in user_portfolio
+    if stock_name in user_portfolio:
+        if user_portfolio[stock_name]['quantity'] >= share_amount:
+            if stock_name in stock_data:
+                stock_price = stock_data[stock_name]
+                total_earned = stock_price * share_amount
+
+                #Add money to user funds
+                user_money += total_earned
+
+                # update user stock
+                user_portfolio[stock_name]['quantity'] -= share_amount
+                #update user portfolio total cost
+                user_portfolio[stock_name]['total_cost'] -= total_earned
+
+                print(f"You have sold {share_amount} of {stock_name} for {stock_price} per share.")
+                print(f"Total earnings: {total_earned}")
+                print(f"Your Remaining total: {user_money}")
+            else:
+                print("Stock not found")
+        else:
+            print(f"You don't have enough shares of {stock_name} to sell at this time.")
+    else:
+        print(f"You don't have any shares of {stock_name}")
+
+    return user_money, user_portfolio
+
+#Shows all the current stock data from the user
+def display_stocks(stock_data):
+    print("Stock Data: ")
+    for stock, price in stock_data.items():
+        print(f"{stock}: {price}")
+
+def observe_portfolio():
+    print("User Portfolio: ")
+    for stock, data in user_portfolio.items():
+        print(f"{stock}: Quantity - {data['quantity']}, Total Cost - {data['total_cost']}")
+
+#Runs the function of loading up stock data
+stock_data = load_stockdata()
+
+#Calling function with loaded stock data
+display_stocks(stock_data)
 
 while True:
     # user prompts
@@ -54,123 +141,31 @@ while True:
     print("6. Exit\n")
     user_input = int(input("Choose a number between 1 - 6: "))
 
-    #Task 1.3 - Show stock name + price
+    #Shows stock name and price
     if user_input == 1:
-        for stock, price in stock_data.items():
-            print(f"The price of {stock} stock is ${price}")
-
-    #Task 2 - Open stock.txt file to read data
-    with open('stock.txt', 'r') as file:
-        #read each line
-        for line in file:
-            #split data in stock name and stock price
-            stock_name, initial_price = line.strip().split(',')
-
-            #store stock data in dictionary
-            if stock_name not in stock_data:
-                stock_data[stock_name] = {'price': float(initial_price), 'quantity': 0}
-
-    #Show stored data
-    print("Stock Data: ")
-    for stock, data in stock_data.items():
-        print(f"{stock}: {data['price']}")
-
-    #Task 3 - Function to buy stock
-    def buy_stock(stock_data, stock_name, share_amount, user_money):
-        if stock_name not in stock_data:
-            print(f"{stock_name} does not exist")
-            return user_money
-
-        #cost calculations for buying stocks
-        stock_price = stock_data[stock_name]['price']
-        total_cost = stock_price * share_amount
-
-        #check if user has enough money
-        if total_cost > user_money:
-            print("You don't have enough money to buy this stock")
-            return user_money
-
-        #deduct money from user funds
-        user_money -= total_cost
-
-        #update user's stock when changes are made
-        stock_data[stock_name]['quantity'] += share_amount
-
-        #update user portfolio
-        if stock_name in user_portfolio:
-            user_portfolio[stock_name]['quantity'] += share_amount
-            user_portfolio[stock_name]['total_cost'] += total_cost
-        else:
-            user_portfolio[stock_name] = {'quantity': share_amount, 'total_cost': total_cost}
-
-        #display how much bought of stock by price and total remaining
-        print(f"You have bought {share_amount} of {stock_name} for {stock_price} per share")
-        print(f"Your remaining total: {user_money}")
-
-        return user_money
-
-    #Task 3.1 - Calling function buy stock
-    if user_input == 2:
-        stock_name = (input("What stock do you want to buy: "))
+       display_stocks(stock_data)
+    elif user_input == 2:
+        stock_name = input("What stock do you want to buy: ")
         share_amount = int(input(f"How much of {stock_name} do you want to buy: "))
-
-        #calling function buy_stock
-        user_money = buy_stock(stock_data, stock_name, share_amount, user_money)
-
-    #Task 4 - Function to sell stock
-    def sell_stock(stock_data, stock_name, share_amount, user_money):
-        if stock_name not in stock_data:
-            print(f"{stock_name} does not exist")
-            return user_money
-
-        if stock_data[stock_name]['quantity'] >= share_amount:
-            stock_price = stock_data[stock_name]['price']
-            total_earned = stock_price * share_amount
-
-            #Add money to user funds
-            user_money += total_earned
-
-            #update user stock
-            stock_data[stock_name]['quantity'] -= share_amount
-
-            print(f"you have sold {share_amount} of {stock_name} for {stock_price} per share.")
-            print(f"Total earnings: {total_earned}")
-            print(f"Your Remaining total: {user_money}")
-        else:
-            print(f"You don't have enough shares of {stock_name} to sell at this time.")
-
-        return user_money
-
-    #Task 4.1 - Calling function sell stock
-    if user_input == 3:
-        stock_name = input(f"What stock do you want to sell? ")
+        user_money, user_portfolio = buy_stock(stock_data, stock_name, share_amount, user_money, user_portfolio)
+    elif user_input == 3:
+        stock_name = input("What stock do you want to sell? ")
         share_amount = int(input(f"How much of {stock_name} do you want to sell? "))
-
-        #calling function sell stock
-        user_money = sell_stock(stock_data, stock_name, share_amount, user_money)
-
-    #Task 5 - View user stocks
-    if user_input == 4:
-        print("Your portfolio results: ")
-        total_portfolio_value = 0
-        for stock, data in user_portfolio.items():
-            #filter out stocks that are greater 0
-            quantity = data['quantity']
-            total_cost = data['total_cost']
-            #print(f"{stock} - Quantity: {quantity}, Total value: {total_value}")
-            print(f"{stock} - Quantity: {quantity}, Total cost: {total_cost}")
-            total_portfolio_value += total_cost
-        print(f"Total portfolio value: {total_portfolio_value}")
-
-    #Task 6 - Save user stocks
-    if user_input == 5:
+        user_money, user_portfolio = sell_stock(stock_data, stock_name, share_amount, user_money, user_portfolio)
+    elif user_input == 4:
+        observe_portfolio()
+    elif user_input == 5:
         save_portfolio(user_portfolio)
         print("Portfolio successfully saved!")
-
-    #Task 7 - Exit message
-    if user_input == 6:
+    elif user_input == 6:
         print("Thank you for checking the Stock Market today!")
         break
+    else:
+        print("Invalid input. Please choose a number between 1 - 6.")
+
+
+
+
 
     '''
     #function example for creating prices
